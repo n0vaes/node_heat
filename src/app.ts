2f6a6a08127d30af09b5c49f67ca4529bb7c80c5
n0vaes <alanovaes@live.com>
2021-10-19 19:22:47 -0300

    a1 finalizada

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server} from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`User connected on socket ${socket.id}`);
+});
 
 app.use(express.json());
 
@@ -19,4 +35,4 @@ app.get("/signin/callback", (request, response) => {
   return response.json(code);
 });
 
-app.listen(4000, () => console.log(`Server is running on PORT 4000`));
+export { serverHttp, io };
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,9 +1,9 @@
+import { io } from "../app";
 import { prismaClient } from "../prisma";
 
 
 class CreateMessageService {
   async execute(text: string, user_id: string) {
-    console.log(text)
     const message = await prismaClient.message.create({
       data: {
         text,
@@ -13,6 +13,18 @@ class CreateMessageService {
         user: true
       },
     });
+
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user.name,
+        avatar_url: message.user.avatar_url,
+      }
+    }
+
+    io.emit("new_message", infoWS)
 
     return message;
   }
