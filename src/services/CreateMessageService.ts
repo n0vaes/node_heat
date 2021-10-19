45e33eded07e9ce2dc9406cdfc4eeb90a3b038fa
n0vaes <alanovaes@live.com>
2021-10-19 14:39:13 -0300

    1st commit

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,21 @@
+import { prismaClient } from "../prisma";
+
+
+class CreateMessageService {
+  async execute(text: string, user_id: string) {
+    console.log(text)
+    const message = await prismaClient.message.create({
+      data: {
+        text,
+        user_id
+      },
+      include: {
+        user: true
+      },
+    });
+
+    return message;
+  }
+}
+
+export { CreateMessageService }
